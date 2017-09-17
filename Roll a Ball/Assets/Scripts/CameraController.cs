b34e699343637126689845d2032c349585b11e53
Barry <barry.a.yang.18@dartmouth.edu>
2017-09-17 17:29:11 -0400

    tutorial finished

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset; // offset is private because you can only set it in script
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+
+	// LateUpdate runs every frame, but guaranteed to run after all items have been updated
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	/*
@@ -17,11 +18,23 @@ public class PlayerController : MonoBehaviour {
 //		
 //	}
 
-	private Rigidbody rb;
+
 	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int score;
+	private GameObject[] cubeArray;
+	private int numCubes;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
+		score = 0;
+		SetCountText ();
+		winText.text = "";
+		cubeArray = GameObject.FindGameObjectsWithTag ("Pick Up");
+		numCubes = cubeArray.Length;
 	}
 
 	// Called just before any fixed calculations, physics code here
@@ -32,5 +45,21 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			score = score + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Score: " + score.ToString ();
+		if (score >= numCubes) {
+			winText.text = "You Win!";
+		}
+			
 	}
 }
